c306ba801cdd331966af8806d34c88a87c5a8b72
knsaicharan <knsaicharan@gmail.com>
2019-08-20 16:09:08 +0530

    first commit

---
+++ src/test/java/practice/test/NewTest.java
@@ -0,0 +1,30 @@
+package practice.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ 
+
+public class NewTest {
+	
+	public String baseUrl = "https://www.google.com";
+	//String driverPath = "F://chromedriver.exe";
+	public WebDriver driver ; 
+
+	@Test
+	public void verifyHomepageTitle() {
+
+		System.out.println("launching chrome browser"); 
+		System.setProperty("webdriver.chrome.driver", "F://chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get(baseUrl);
+		String expectedTitle = "Google";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+		driver.close();
+	}
+}
+
+
